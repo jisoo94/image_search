2c01f128ac70b2a47900bcaddf89f37ae69559d0
jisoo94 <max5751@naver.com>
2023-09-29 21:11:29 +0900

    image start

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:image_search/data/api.dart';
+import 'package:image_search/data/photo_provider.dart';
+import 'package:image_search/ui/home_creen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: PhotoProvider(api: PixabatApi(), child: const HomeScreen()),
+    );
+  }
+}
