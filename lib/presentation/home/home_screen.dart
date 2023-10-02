44bf1d258c53df7c78c5951b64eb1b44f031e5a2
jisoo94 <max5751@naver.com>
2023-10-02 17:06:49 +0900

    상태를 불변상태로 관리하기

--- lib/presentation/home/home_screen.dart
+++ lib/presentation/home/home_screen.dart
@@ -43,6 +43,7 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     final viewModel = context.watch<HomeViewModel>();
+    final state = viewModel.state;
 
     return Scaffold(
       appBar: AppBar(
@@ -71,22 +72,25 @@ class _HomeScreenState extends State<HomeScreen> {
                       icon: const Icon(Icons.search))),
             ),
           ),
-          Expanded(
-            child: GridView.builder(
-                padding: const EdgeInsets.all(16),
-                itemCount: viewModel.photos.length,
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  crossAxisSpacing: 16,
-                  mainAxisSpacing: 16,
-                ),
-                itemBuilder: (context, index) {
-                  final photo = viewModel.photos[index];
-                  return PhotoWidget(
-                    photo: photo,
-                  );
-                }),
-          )
+          state.isLoading
+              ? const CircularProgressIndicator()
+              : Expanded(
+                  child: GridView.builder(
+                      padding: const EdgeInsets.all(16),
+                      itemCount: state.photos.length,
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        crossAxisSpacing: 16,
+                        mainAxisSpacing: 16,
+                      ),
+                      itemBuilder: (context, index) {
+                        final photo = state.photos[index];
+                        return PhotoWidget(
+                          photo: photo,
+                        );
+                      }),
+                )
         ],
       ),
     );
